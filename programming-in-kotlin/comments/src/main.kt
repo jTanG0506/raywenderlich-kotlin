d3f07c3859219da7733f25f4c980dbbacdafbc4e
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-28 21:17:28 +0100

    Complete 'comments'

---
+++ programming-in-kotlin/comments/src/main.kt
@@ -0,0 +1,18 @@
+fun main(args : ArrayList<String>) {
+    // This is a comment. It is not executed.
+
+    // This is also a comment.
+    // Over multiple lines.
+
+    /*  This is a comment.
+        Over many..
+        many..
+        many lines..
+     */
+
+    /*  This is a comment
+    /*  And inside it is another comment.
+     */
+     *  Back to the first.
+     */
+}
